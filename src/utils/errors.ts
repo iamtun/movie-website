fa208c37bb04d2682b392c0dfbcd6eb73a12d387
iamtun <letuan19431791.iuh@gmail.com>
2024-10-16 14:27:54 +0700

    feat(layout): create handle error in catch

--- src/utils/errors.ts
+++ src/utils/errors.ts
@@ -1,16 +1,35 @@
-class APIError extends Error {
+import { notFound } from "next/navigation";
+
+export interface IAPIError extends Error {
+	status: number;
+}
+
+class APIError extends Error implements IAPIError {
 	public status: number;
 
 	constructor(message: string, status: number) {
-		super(message); // Call the parent class (Error) constructor
+		super(message);
 		this.status = status;
-		this.name = "APIError"; // Set the name of the error class
+		this.name = "APIError";
 
-		// Maintains proper stack trace (for V8-based environments like Node.js)
 		if (Error.captureStackTrace) {
 			Error.captureStackTrace(this, this.constructor);
 		}
 	}
 }
 
-export { APIError };
+const handleErrorInCatch = (error: unknown) => {
+	if (error instanceof APIError) {
+		if (error.status === 404) {
+			notFound();
+		}
+
+		throw new APIError(error.message, error.status);
+	}
+
+	if (error instanceof Error) {
+		throw new Error(error.message);
+	}
+};
+
+export { APIError, handleErrorInCatch };
